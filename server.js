a3c178362a9f91f0ad75853b5d5d9cc3533eeb88
Sam Silver <sam@Sams-MacBook-Air.local>
2022-01-21 15:24:20 -0400

    Add models

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect to Database
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("Main page"));
 
 // Define Routes
