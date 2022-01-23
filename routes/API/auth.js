17ab4c2deaf464c7e6c0b1a509084ddf062e6877
sam55silver <sam55silver@gmail.com>
2022-01-23 17:03:45 -0400

    Started adding authentication with jwt... also setup windows workstation for project

--- routes/API/auth.js
+++ routes/API/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
 router = express.Router();
+const auth = require("../../middleware/auth");
 
 // @route   GET API/auth
 // @desc    Test route
 // @access  Public
-router.get("/", (req, res) => {
+router.get("/", auth, (req, res) => {
   res.send("Auth route");
 });
 
